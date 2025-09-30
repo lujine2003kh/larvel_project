e35fdd76ca2eb3a41b342d642a7c2dd1e5b1e249
lujine kh <115995189+lujine2003kh@users.noreply.github.com>
2025-09-30 15:50:03 +0300

    Initial commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Employee::all());
+    }
+
+    public function store(Request $request)
+    {
+        $employee = Employee::create($request->all());
+        return response()->json($employee, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $employee = Employee::findOrFail($id);
+        $employee->update($request->all());
+        return response()->json($employee);
+    }
+
+    public function destroy($id)
+    {
+        Employee::destroy($id);
+        return response()->json(['message' => 'Deleted successfully']);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+
+Route::get('/employees', [EmployeeController::class, 'index']);
+Route::post('/employees', [EmployeeController::class, 'store']);
+Route::put('/employees/{id}', [EmployeeController::class, 'update']);
+Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+
+// Route::get('/test', function() {
+//     return response()->json(['message' => 'API works!']);
+// });
